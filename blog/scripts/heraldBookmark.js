6b24179e2e384db02f0251e4e56e91025c29a383
gastongouron <gastongourogmail.l.com>
2015-09-11 14:57:32 +0200

    Improve code indent

--- blog/scripts/heraldBookmark.js
+++ blog/scripts/heraldBookmark.js
@@ -4,8 +4,9 @@ $('#heraldIcon').click(function(e) {
     var triggerDefault = false;
 
     if (window.sidebar && window.sidebar.addPanel) {
-        // Firefox version < 23
-        window.sidebar.addPanel(bookmarkTitle, bookmarkURL, '');
+    // Firefox version < 23
+    window.sidebar.addPanel(bookmarkTitle, bookmarkURL, '');
+
     } else if ((window.sidebar && (navigator.userAgent.toLowerCase().indexOf('firefox') > -1)) || (window.opera && window.print)) {
         // Firefox version >= 23 and Opera Hotlist
         var $this = $(this);
@@ -14,12 +15,14 @@ $('#heraldIcon').click(function(e) {
         $this.attr('rel', 'sidebar');
         $this.off(e);
         triggerDefault = true;
+
     } else if (window.external && ('AddFavorite' in window.external)) {
-        // IE Favorite
-        window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+    // IE Favorite
+    window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+
     } else {
-        // WebKit - Safari/Chrome
-        alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+    // WebKit - Safari/Chrome
+    alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
     }
 
     return triggerDefault;
