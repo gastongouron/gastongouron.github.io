fb179180ffa1ced674c99069cf325688538fc43e
gastongouron <gastongouron@gmail.com>
2016-03-21 17:34:59 +0100

    talent?

---
+++ talent/README.md
@@ -0,0 +1,3 @@
+# oldgastongouron.github.io
+
+A copy of my old website
---
+++ talent/js/script.js
@@ -0,0 +1,35 @@
+/* Author: 
+
+*/
+$(".projetmenu").hover(
+	function(){
+		//$(this).children('.projetmenuin').animate({'left':"-150px"},'fast')
+		$(this).children('.projetmenuin').css({'left':"-150px"})
+	},
+	function(){
+		//$(this).children('.projetmenuin').animate({'left':"0px"},'fast')
+		$(this).children('.projetmenuin').css({'left':"0px"})
+	}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
